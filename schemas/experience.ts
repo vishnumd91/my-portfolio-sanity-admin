380b7268050cc6b890e659ae141059328c8dd750
Vishnu MD <vishnudivakaran1@gmail.com>
2022-12-31 20:41:20 +0530

    added experience schema and removed unwanted schemas

---
+++ schemas/experience.ts
@@ -0,0 +1,79 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'experience',
+  title: 'Experience',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'jobRole',
+      title: 'Job Role',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+        slugify: input => input
+                         .toLowerCase()
+                         .replace(/\s+/g, '-')
+                         .slice(0, 200)
+      },
+    }),
+    defineField({
+      name: 'companyName',
+      title: 'Company Name',
+      type: 'string'
+    }),
+    defineField({
+      name: 'state',
+      title: 'State',
+      type: 'string'
+    }),
+    defineField({
+      name: 'country',
+      title: 'Country',
+      type: 'string'
+    }),
+    defineField({
+      name: 'description',
+      title: 'Job Description',
+      type: 'blockContent',
+    }),
+    defineField({
+      name: 'startDate',
+      title: 'Start Date',
+      type: 'date',
+    }),
+    defineField({
+      name: 'endDate',
+      title: 'End Date',
+      type: 'date' ,
+      hidden: ({ parent, value }) => value && parent?.current
+    }),
+    defineField({
+      name: 'current',
+      title: 'Currently Working here',
+      type: 'boolean'
+    })
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
